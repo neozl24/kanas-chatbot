33b191ed33672b7eeab599d2e6b5407e6b0eaf20
Zhong Li <neozl24@hotmail.com>
2024-10-09 23:53:31 +0800

    prepare the basic constructure for a Chatbot library

---
+++ lib/ChatWindow/ChatWindow.tsx
@@ -0,0 +1,3 @@
+export const ChatWindow = () => {
+  return <div>I will be Chat Window</div>
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'lib/main.ts'),
+      name: 'kanas-chatbot',
+      fileName: 'kanas-chatbot',
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom', 'react/jsx-runtime'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+          'react/jsx-runtime': 'react/jsx-runtime',
+        },
+      }
+    },
+  },
 })
