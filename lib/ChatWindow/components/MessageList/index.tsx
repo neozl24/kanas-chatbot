0a14bffe3628a00d351f27b757df6b5db6874c50
Zhong Li <neozl24@hotmail.com>
2024-10-15 03:57:58 +0800

    add ui for message list

--- lib/ChatWindow/components/MessageList/index.tsx
+++ lib/ChatWindow/components/MessageList/index.tsx
@@ -1,23 +1,62 @@
 // import { useState } from 'react'
+import defaultAiIcon from '../../../assets/ai.svg'
+import { Message } from '../../utils/types'
 
 import './index.css'
 
 interface MessageListProps {
-  list?: string[]
+  list: Message[]
+  aiLogo?: React.ReactNode
 }
 
 export const MessageList = ({
   list,
+  aiLogo,
 }: MessageListProps) => {
-  
-
   return (
     <div className='chat-window-message-list'>
       {
         list?.map((item) => (
-          item
+          item.role === 'assistant' ?
+            <AssistantMessage logo={aiLogo} content={item.content} /> :
+            <UserMessage content={item.content} />
         ))
       }
+    </div>
+  )
+}
+
+const AssistantMessage = ({
+  content,
+  logo,
+}: {
+  content: string
+  logo?: React.ReactNode
+}) => {
+  const defaultAiLogo = <img className='default-assistant-logo' src={defaultAiIcon} alt='ai logo' width={30} />
+
+  return (
+    <div className='assistant-message-container'>
+      <div className='assistant-logo-area'>
+        {logo || defaultAiLogo}
+      </div>
+      <div className='assistant-message-area'>
+        <div className='assistant-message-content'>{content}</div>
+      </div>
+    </div>
+  )
+}
+
+const UserMessage = ({
+  content,
+}: {
+  content: string
+}) => {
+  return (
+    <div className='user-message-container'>
+      <div className='user-message-area'>
+        <div className='user-message-content'>{content}</div>
+      </div>
     </div>
   )
 }
--- lib/ChatWindow/index.tsx
+++ lib/ChatWindow/index.tsx
@@ -4,6 +4,7 @@ import { MessageList } from './components/MessageList'
 import { BottomArea } from './components/BottomArea'
 
 import './index.css'
+import { Message } from './utils/types'
 
 interface ChatWindowProps {
   title: string
@@ -11,6 +12,7 @@ interface ChatWindowProps {
   style?: CSSProperties
   topChildren?: React.ReactNode
   bottomChildren?: React.ReactNode
+  aiLogo?: React.ReactNode
 }
 
 export const ChatWindow = ({
@@ -19,15 +21,31 @@ export const ChatWindow = ({
   style,
   topChildren,
   bottomChildren,
+  aiLogo,
 }: ChatWindowProps) => {
   const submitInput = (text: string) => {
     console.log('submit: ', text)
   }
 
+  const messages: Message[] = [
+    {
+      role: 'assistant', 
+      content: 'Can I assist you with any questions or concerns you might have?',
+    },
+    {
+      role: 'user', 
+      content: 'Is free plan available?',
+    },
+    {
+      role: 'assistant', 
+      content: 'Let me see...',
+    },
+  ]
+
   return (
     <div className='chat-window-container' style={style}>
       <Header title={title} onClose={onClose} topChildren={topChildren} />
-      <MessageList />
+      <MessageList list={messages} aiLogo={aiLogo} />
       <BottomArea submitInput={submitInput} bottomChildren={bottomChildren} />
     </div>
   )
